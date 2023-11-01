2d11319210772451889b88abdd5e2bb66ea9d70c
Jay Rathod <124586189+JayRathod-99@users.noreply.github.com>
2023-11-01 15:19:08 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = true;
+
+console.log(typeof score)
+console.log(typeof(score))
+
+
+let changeToNum = Number(score)
+console.log(typeof changeToNum);
+console.log(changeToNum);
+
+
+// "33" => 33
+// true => 1 ; false => 0
+//"22asdf"  => NaN
+ 
+
+let isLoggedIn = 1
+let checkBoolean = Boolean(isLoggedIn)
+console.log(checkBoolean);
+
+
+let num  = 22;
+let numToStr = String(num)
+console.log(numToStr);
+console.log(typeof numToStr);
